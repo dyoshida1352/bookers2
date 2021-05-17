43f44c228768b6f577bc73b0bb866006c20cf2d0
dyoshida1352 <d.yoshida1352@gmail.com>
2021-05-17 13:29:03 +0000

    [Add]booksの機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   resources :homes, only: [:index]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+
 end
