a54fd2917177487400b5826c28a4bf4cc17474c4
dyoshida1352 <d.yoshida1352@gmail.com>
2021-05-21 05:05:31 +0000

    [Add]機能の実装完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,20 +2,27 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    book.save
-    redirect_to book_path(book)
+    @user = current_user
+    @book = Book.new(book_params)
+    @books = Book.all
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      render :index
+    end
   end
 
 
   def show
-    @book = Book.new
-    @books = Book.find(params[:id])
-
+    @new_book = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def destroy
@@ -29,9 +36,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,26 @@
 class UsersController < ApplicationController
   def index
-    @book = Book.new
-    @user = User.all
+    @new_book = Book.new
+    @users = User.all
+    @user = current_user
   end
 
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    book.save
-    redirect_to book_path(book)
+    @user = current_user
+    @book = Book.new(book_params)
+    @books = Book.all
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      render 'books/index'
+    end
   end
 
 
   def show
-    @book = Book.new
+    @new_book = Book.new
     @user = User.find(params[:id])
     @books = @user.books.order('created_at DESC')
   end
@@ -24,8 +31,12 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user)
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   private
