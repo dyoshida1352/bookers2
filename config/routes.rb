42d7dc09db132c56614d3a74b8fdc724e6960618
dyoshida1352 <d.yoshida1352@gmail.com>
2021-06-05 03:16:20 +0000

    [Add]検索機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   get "/home/about" => "homes#about"
+  get 'search/search'
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
